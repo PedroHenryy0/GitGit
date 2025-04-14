d12b2a152bdefed60cef7655fab577efd69776b3
Pedro Henry <seuemail@exemplo.com>
2025-04-14 20:54:45 -0300

    Números Ímpares

---
+++ Gitlit/src/Exerc5.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Exerc4 {
+    public static void main(String[] args) {
+
+        //4. Números Pares até N
+        for (int l = 1; l <= 50; l++) { // While l <= 50, add +1
+            if (l % 2 == 0) { // If the value of l % 2 = 0 , is true
+                System.out.println("Números Pares: " + l); // Print "pares" numbers
+            }
+
+
+        }
+    }
+}
