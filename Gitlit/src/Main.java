8586c0b1d4d6462817cbbcf7f08fbfa6e4fbba90
PedroHenry <132282311+PEKOPECORINE@users.noreply.github.com>
2025-04-13 23:28:54 -0300

    Dados inciais

---
+++ Gitlit/src/Main.java
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+
+        // Exercícios de Java de Ivens Diego feito por Pedro H.S.M
+    }
+}
