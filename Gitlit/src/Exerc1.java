90ffa6cbee332c19e71b7887179bfa1aa8a02ce2
PedroHenry <132282311+PEKOPECORINE@users.noreply.github.com>
2025-04-13 23:36:53 -0300

    Exercício 1 - Contagem de 1 a 10

---
+++ Gitlit/src/Exerc1.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Exerc1 {
+    public static void main(String[] args) {
+
+        // 1.  Contagem de 1 a 10 ---
+        for (int i = 0 ; i <= 10; i++) { // While i <= 10, add +1
+            System.out.println("Contagem de 1 a 10: " + i);} // Print the nums
+    }
+}
