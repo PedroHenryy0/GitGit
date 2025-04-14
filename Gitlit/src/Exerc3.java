516c4d9caeeb54bff5a18a0385c375d8cca121ab
Pedro Henry <seuemail@exemplo.com>
2025-04-14 20:28:53 -0300

    Tabuada de um Número

---
+++ Gitlit/src/Exerc3.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Exerc3 {
+    public static void main(String[] args) {
+
+        //3. Tabuada de Número
+        int val2 = 5;
+            for (int k = 1;k <= 10; k++){ // While k <= 10, add +1
+                int bau = (val2) * k; // Variable * with k every time that k receive a new value
+                    System.out.println("Tabuada do 5: " + bau);} // Print result
+    }
+}
