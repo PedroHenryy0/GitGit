bf10e50d8623872d32e19627c58966d1b7c5c4b9
Pedro Henry <seuemail@exemplo.com>
2025-04-14 20:22:48 -0300

    Contagem de 1 a N

---
+++ Gitlit/src/Exerc2.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Exerc2 {
+    public static void main(String[] args) {
+
+        // 2. Soma de 1 a N
+        int val1 = 5;
+        for (int j = 5; j >= 1; --j){
+            int div = val1 += (j - 1);}
+        System.out.println("Resultado da Soma dos Elementos:" + val1);
+    }
+}
