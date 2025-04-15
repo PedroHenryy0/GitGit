6772ffb95cbc24bad82368e92b982cce8ade9236
Pedro Henry <seuemail@exemplo.com>
2025-04-14 21:23:49 -0300

    Soma dos Dígitos de um Número

---
+++ Gitlit/src/Exerc7.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Exerc7 {
+    public static void main(String[] args) {
+
+        //7. Soma dos Dígitos de um Número
+        int val4 = 15;
+        double val5 = val4 / 10; // 15 / 10 = 1.5
+        double val6 = val4 % 10; // Pick the "5" of 1.5
+        double val7 = (val5 + val6); // 1 + 5 = 6
+        System.out.println("Soma dos Dígitos de um Número: " + val7); // Print number
+    }
+    }
